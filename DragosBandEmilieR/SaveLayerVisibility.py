1f71e143228f265c40fc04cfcb7ac5e9a28ad59b
DESKTOP-7EES3O2\Emilie Rabeau <mie_rabeau@rogers.com>
2018-02-22 16:06:04 -0500

    savlayervisibilty works well! prints to the txt file

---
+++ DragosBandEmilieR/SaveLayerVisibility.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        SaveLayerVisibility
+# Purpose:
+#
+# Author:      Emilie Rabeau
+#
+# Created:     22-02-2018
+# Copyright:   (c) Emilie Rabeau 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import arcpy
+import os
+
+visibilityFile = open(r'LayerVisibility.txt', 'w')
+
+mxd = arcpy.mapping.MapDocument(r"..\..\MappingEx.mxd")
+dataFrame = arcpy.mapping.ListDataFrames(mxd)
+
+
+for data in dataFrame:
+    layer = arcpy.mapping.ListLayers(data)
+    for lyr in layer:
+        visibilityFile.write("{:15}\t {:10}\t {:10}\n".format(data.name, lyr.name, lyr.visible))
+
+visibilityFile.close()
