b5dcdd06916e860bc22545e7b3b9bcab17cfaa58
DESKTOP-7EES3O2\Emilie Rabeau <mie_rabeau@rogers.com>
2018-02-23 10:14:08 -0500

    no message

---
+++ DragosBandEmilieR/mapping04.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        mapping04.py
+# Purpose:     Print a report of the layer information, including x, y
+# coordinates.
+#
+# Author:      Emilie Rabeau, Dragos B
+#
+# Created:     22-02-2018
+# Copyright:   (c) Emilie Rabeau 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
