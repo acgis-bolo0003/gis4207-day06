bacadb30133551578709077bbd979b592af97032
Dragos Bologa <bolo0003@algonquinlive.com>
2018-02-25 10:58:24 -0500

    DB: Fixed Mapping07.py (changed hardcoded paths using os.path.abspath instead)

--- DragosBandEmilieR/Mapping07.py
+++ DragosBandEmilieR/Mapping07.py
@@ -10,20 +10,37 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import arcpy
-#mxd = arcpy.mapping.MapDocument(r".\mxdTemp\MappingEx.mxd")
-mxd = arcpy.mapping.MapDocument(r"D:\acgis\gis4207_Customization_I\day06\lab\DragosBandEmilieR\mxdTemp\MappingEx.mxd")
+import sys
+import os
+#import os.path as path
+mxd = arcpy.mapping.MapDocument(r".\mxdTemp\MappingEx.mxd")
+#mxd = arcpy.mapping.MapDocument(r"D:\acgis\gis4207_Customization_I\day06\lab\DragosBandEmilieR\mxdTemp\MappingEx.mxd")
 
+scriptPath = os.path.dirname(__file__)
+os.chdir(scriptPath)
+# print("Absolute path of the current script: "+ os.path.join(os.path.dirname(__file__)))
+
+# Path three levels up from the current script (to get to broken links path):
+three_up =  os.path.abspath(path.join(__file__ ,"../../.."))
+# print("Path three levels up from the current script: "+ three_up)
+
+# Path four levels up from the current script (to get to correct data link path):
+four_up =  os.path.abspath(path.join(__file__ ,"../../../.."))
+#print("Path four levels up from the current script: "+ four_up)
+
+# List Broken DataSources links:
 for lyr in arcpy.mapping.ListBrokenDataSources(mxd):
-    print "Datasource %s does not exist" % lyr.dataSource
+    print "Broken link before fixing path. Datasource %s does not exist" % lyr.dataSource
 
-#mxd.findAndReplaceWorkspacePaths(r"..\..\data", r"..\..\..\Data")
-mxd.findAndReplaceWorkspacePaths(r"D:\acgis\gis4207_Customization_I\day06\data", r"D:\acgis\gis4207_Customization_I\data")
-print "Path replaced"
+#findAndReplaceWorkspacePaths broken paths e.g. "D:\acgis\gis4207_Customization_I\day06\data" change to "D:\acgis\gis4207_Customization_I\data")
+mxd.findAndReplaceWorkspacePaths(three_up +"\data", four_up + "\data")
+# print "Broken links Path: " + three_up +"\data"+ "  replaced with correct path: "+ four_up + "\data"
 
+# List again broken DataSources links (should not get broken links after fixing path):
 for lyr in arcpy.mapping.ListBrokenDataSources(mxd):
     print "Datasource %s does not exist" % lyr.dataSource
 else:
-    print "No broken links"
+    print "No broken links after fixing path"
 
 #mxd.saveACopy(r"D:\acgis\gis4207_Customization_I\day06\lab\DragosBandEmilieR\mxdTemp\MappingExNewLinks.mxd")
 mxd.save()
