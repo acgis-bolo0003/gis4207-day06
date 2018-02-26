81bb18ec70bff90e5a7d1718c513f0cba6a6fd2d
Dragos Bologa <bolo0003@algonquinlive.com>
2018-02-26 09:14:04 -0500

    DB: Pushed Mapping04 (no errors however visibility update to True happens for all layers not only Point shapeType

--- DragosBandEmilieR/mapping04.py
+++ DragosBandEmilieR/mapping04.py
@@ -5,27 +5,39 @@
 #
 # Author:      DragosB, EmilieR
 #
-# Created:     22-02-2018
+# Created:     26-02-2018
 # Copyright:   (c) DragosB, EmilieR, 2018
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import arcpy
 import os
+#mxd = arcpy.mapping.MapDocument("MappingEx.mxd")
+mxd = arcpy.mapping.MapDocument("CURRENT")
 
-mxd = arcpy.mapping.MapDocument("MappingEx.mxd")
-#mxd = arcpy.mapping.MapDocument("CURRENT")
+# Path four levels up from the current script (to get to correct data link path):
+#four_up =  os.path.abspath(os.path.join(__file__ ,"../../../.."))
+#print("Path four levels up from the current script: "+ four_up)
+#workspace_path = four_up + "\data\Canada\Canada.gdb"
+#workspace_path = four_up + "\data"
+#arcpy.env.workspace = workspace_path
 
-for df in arcpy.mapping.ListDataFrames(mxd):
-  for ly in arcpy.mapping.ListLayers(mxd, "", df):
-     descly = arcpy.Describe(ly)
-     geom = descly.ShapeType
-     if geom =='point':
-        ly.visible=='True'
-     else:
-        ly.visible=='False'
-
+dataframes = arcpy.mapping.ListDataFrames(mxd)
+layers = arcpy.mapping.ListLayers(mxd)
+fcList = arcpy.ListFeatureClasses()
+print fcList
+point_features = list()
+for layer in layers:
+    for feature in fcList:
+        desc = arcpy.Describe(feature)
+        if desc.shapeType == 'Point':
+                point_features.append(feature)
+                print("Point Layer before update " + layer.name + " is visible:       " + str(layer.visible))
+                layer.visible = True
+                #arcpy.mapping.UpdateLayer(df,layer, desc.shapeType,True)
+                print("Point Layer after update " + layer.name + " is visible:       " + str(layer.visible))
+        else:
+                layer.visible = False
 arcpy.RefreshActiveView()
 arcpy.RefreshTOC()
-
-mxd.save()
 del mxd
+
